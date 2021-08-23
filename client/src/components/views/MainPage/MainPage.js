9ee7497333ab399e358ea1219423f4171747c243
kimyu0218 <serena35@naver.com>
2021-08-24 00:36:52 +0900

    main page...

--- client/src/components/views/MainPage/MainPage.js
+++ client/src/components/views/MainPage/MainPage.js
@@ -1,40 +1,67 @@
 import React, { useState, useEffect } from 'react'
 import { withRouter } from "react-router-dom"
+import Axios from 'axios';
 
-import { Input } from 'antd'
+import { Input, InputNumber } from 'antd'
 import { API_URL, API_KEY, PROXY_SERVER } from './Sections/currency_api'
-import CountryInfo from './Sections/CountryInfo'
 
 function MainPage() {
 
     const [Country, setCountry] = useState("")
-    const [Currency, setCurrency] = useState()
+    const [Currency, setCurrency] = useState(0)
     const [Result, setResult] = useState([])
 
     const { Search } = Input;
     const onCountryHandler = (event) => { setCountry(event.currentTarget.value); }
     const onCurrencyHandler = (event) => { setCurrency(event.currentTarget.value); }
-    const onResultHandler = (event) => { setResult(event.currentTarget.value); }
 
     useEffect( () => {
         const endpoint = `${PROXY_SERVER}${API_URL}?authkey=${API_KEY}&data=AP01`
-        console.log(endpoint)
-        //fetchCurrency(endpoint)
+        getCurrency(endpoint)
     }, []);
 
+    const getCurrency = (endpoint) => {
+        Axios.get(endpoint, {})
+            .then(response => {
+                console.log(response)
+                setResult(response.data)
+                console.log("Result: ", Result)
+            })
+    }
+
+    const onSearch = () => {
+        Result.sort()
+        let find = Result.find(country => country.cur_nm===Country)
+        setCurrency(find)
+        console.log(find)
+    }
 
     return (
         <div style={{
             display: 'flex', justifyContent: 'center',
             width: '100%', height: '100vh'
-        }}>
-            <Search
-                style={{ width: '350px', margin: '10px'}}
-                placeholder="country"
-                value={Country}
-                onChange={onCountryHandler}
-                //onSearch={getCurrency}
-            />
+        }}> 
+            <div>
+                {Result &&
+                    <Search
+                        style={{ width: '350px', margin: '10px'}}
+                        placeholder="country"
+                        value={Country}
+                        onChange={onCountryHandler}
+                        onSearch={onSearch}
+                    />
+                }
+            </div>
+            <div>
+                <InputNumber
+                    style={{ width: '350px', margin: '10px'}}
+                    step="0.000000001"
+                    readOnly={true}
+                    value={Currency}
+                    onChange={onCurrencyHandler}
+                    //stringMode
+                />
+            </div>
         </div>
     )
 }
--- client/src/components/views/MainPage/Sections/CountryInfo.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default class CountryInfo extends React.Component {
-    render() {
-        return (
-            <div onClick={this.props.onClick}>{this.props.country.name}</div>
-        )
-    }
-}
